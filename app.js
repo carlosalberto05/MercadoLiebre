130d1b6a09b2fd6010be3e9324cc4141b9c6839d
Carlos <carlos_alberto_lira@hotmail.com>
2021-11-18 09:16:25 -0600

    Se realizaron modificaciones

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
+//   console.log("Servidor corriendo en el puerto 3000");
+// });
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
